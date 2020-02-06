c9a3ab46d98fc013456d4a6d3e3de3440b6e9933
tonydicker1028 <tonydicker1028@outlook.com>
2020-02-05 16:49:53 -0800

    styles

--- client/src/components/restaurants/Restaurant.js
+++ client/src/components/restaurants/Restaurant.js
@@ -14,24 +14,27 @@ const Restaurant = () => {
                     <h1 className='text-center'>{restaurants[0].name}</h1>
                     <img
                         src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRefs[0]}&key=${process.env.REACT_APP_API_KEY}`}
-                        className='img-responsive'
+                        className='mx-auto d-block'
                     />
+
                     <div className='text-center'>Address</div>
                     <p className='text-center'>{restaurants[0].vicinity}</p>
-                    <div className='row'>
-                        <div className='col-sm text-center'>Price</div>
-                        <div className='col-sm text-center'>Rating</div>
-                        <div className='col-sm text-center'>Total Reviews</div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-sm text-center'>
-                            {restaurants[0].price_level === 2 && '$$'}
-                        </div>
-                        <div className='col-sm text-center'>
-                            {restaurants[0].rating} / 5
-                        </div>
-                        <div className='col-sm text-center'>
-                            {restaurants[0].user_ratings_total}
+                    <div className='card'>
+                        <div className='row'></div>
+
+                        <div className='row row-cols-3'>
+                            <div className='col text-center'>Price</div>
+                            <div className='col text-center'>Rating</div>
+                            <div className='col text-center'>Total Reviews</div>
+                            <div className='col text-center'>
+                                {restaurants[0].price_level === 2 && '$$'}
+                            </div>
+                            <div className='col text-center'>
+                                {restaurants[0].rating} / 5
+                            </div>
+                            <div className='col text-center'>
+                                {restaurants[0].user_ratings_total}
+                            </div>
                         </div>
                     </div>
                 </>
