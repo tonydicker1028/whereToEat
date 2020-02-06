69532734d819e479510ff9721716182ec1469a8d
tonydicker1028 <tonydicker1028@outlook.com>
2020-02-05 17:12:18 -0800

    styled btns

--- client/src/components/restaurants/RestaurantButtons.js
+++ client/src/components/restaurants/RestaurantButtons.js
@@ -3,8 +3,8 @@ import React from 'react';
 const RestaurantButtons = () => {
     return (
         <div className='mx-auto mt-4 mb-2'>
-            <button className='btn btn-danger btn-lg mx-5'>Pass</button>
-            <button className='btn btn-success btn-lg'>Lets Go</button>
+            <button className='btn btn-danger btn-lg mr-5'>Pass</button>
+            <button className='btn btn-success btn-lg ml-5'>Lets Go</button>
         </div>
     );
 };
