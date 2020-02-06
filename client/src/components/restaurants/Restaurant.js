f9ad86ba6fca0cc39ae514ecdedd1be74a9214d3
tonydicker1028 <tonydicker1028@outlook.com>
2020-02-05 23:50:12 -0800

    details component

--- client/src/components/restaurants/Restaurant.js
+++ client/src/components/restaurants/Restaurant.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import RestaurantButtons from './RestaurantButtons';
+import RestaurantDetails from './RestaurantDetails';
 
 import RestaurantContext from '../../context/restaurant/restaurantContext';
 
@@ -23,41 +24,9 @@ const Restaurant = () => {
                         src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${restaurants[0].photos[0].photo_reference}&key=${process.env.REACT_APP_API_KEY}`}
                         className='mx-auto d-block'
                     />
-
-                    <div className='my-2'>
-                        <div className='text-center font-weight-bold'>
-                            Address
-                        </div>
-                        <p className='text-center'>{restaurants[0].vicinity}</p>
-                    </div>
-
-                    <div className='row row-cols-3'>
-                        <div className='col text-center font-weight-bold'>
-                            Price
-                        </div>
-                        <div className='col text-center font-weight-bold'>
-                            Rating
-                        </div>
-                        <div className='col text-center font-weight-bold'>
-                            Total Reviews
-                        </div>
-                        <div className='col text-center'>
-                            {restaurants[0].price_level === 0 && '$'}
-                            {restaurants[0].price_level === 1 && '$$'}
-                            {restaurants[0].price_level === 2 && '$$$'}
-                            {restaurants[0].price_level === 3 && '$$$$'}
-                            {restaurants[0].price_level === 4 && '$$$$$'}
-                            {!restaurants[0].price_level && 'N/A'}
-                        </div>
-                        <div className='col text-center'>
-                            {restaurants[0].rating} / 5
-                        </div>
-                        <div className='col text-center'>
-                            {restaurants[0].user_ratings_total}
-                        </div>
-                    </div>
                 </>
             )}
+            <RestaurantDetails />
             <RestaurantButtons />
         </div>
     );
--- client/src/components/restaurants/RestaurantDetails.js
+++ client/src/components/restaurants/RestaurantDetails.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import RestaurantContext from '../../context/restaurant/restaurantContext';
 
 const RestaurantDetails = () => {
-    return <div></div>;
+    const restaurantContext = useContext(RestaurantContext);
+
+    const { restaurantDetails, restaurants } = restaurantContext;
+
+    return (
+        <div className='card my-5'>
+            <div className='row row-cols-3'>
+                <div className='col text-center font-weight-bold'>Address</div>
+                <div className='col text-center font-weight-bold'>
+                    Phone Number
+                </div>
+                <div className='col text-center font-weight-bold'>Website</div>
+                <div className='col text-center'>
+                    {restaurantDetails.formatted_address}
+                </div>
+                <div className='col text-center'>
+                    {restaurantDetails.formatted_phone_number}
+                </div>
+                <d className='col text-center'>
+                    <a href={restaurantDetails.website} target='_blank'>
+                        {restaurantDetails.website}
+                    </a>
+                </d>
+
+                {restaurants.length > 0 && (
+                    <>
+                        <div className='col text-center font-weight-bold'>
+                            Price
+                        </div>
+                        <div className='col text-center font-weight-bold'>
+                            Rating
+                        </div>
+                        <div className='col text-center font-weight-bold'>
+                            Total Reviews
+                        </div>
+                        <div className='col text-center'>
+                            {restaurants[0].price_level === 0 && '$'}
+                            {restaurants[0].price_level === 1 && '$$'}
+                            {restaurants[0].price_level === 2 && '$$$'}
+                            {restaurants[0].price_level === 3 && '$$$$'}
+                            {restaurants[0].price_level === 4 && '$$$$$'}
+                            {!restaurants[0].price_level && 'N/A'}
+                        </div>
+                        <div className='col text-center'>
+                            {restaurants[0].rating} / 5
+                        </div>
+                        <div className='col text-center'>
+                            {restaurants[0].user_ratings_total}
+                        </div>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default RestaurantDetails;
