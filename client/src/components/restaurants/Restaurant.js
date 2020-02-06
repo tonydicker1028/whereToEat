be70664e1b7157c15e0c14ca5a853b38e96ee72c
tonydicker1028 <tonydicker1028@outlook.com>
2020-02-05 23:57:40 -0800

    image styling

--- client/src/components/restaurants/Restaurant.js
+++ client/src/components/restaurants/Restaurant.js
@@ -21,8 +21,8 @@ const Restaurant = () => {
                 <>
                     <h1 className='text-center'>{restaurants[0].name}</h1>
                     <img
-                        src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${restaurants[0].photos[0].photo_reference}&key=${process.env.REACT_APP_API_KEY}`}
-                        className='mx-auto d-block'
+                        src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=400&photoreference=${restaurants[0].photos[0].photo_reference}&key=${process.env.REACT_APP_API_KEY}`}
+                        className='mx-auto d-block restaurantImg'
                     />
                 </>
             )}
