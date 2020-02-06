55e89a9e221cfb06886cfe29c5eb0513c8a994ff
tonydicker1028 <tonydicker1028@outlook.com>
2020-02-05 16:35:03 -0800

    style changes

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
     }, []);
 
     return (
-        <div>
+        <div className='container'>
             <Restaurant />
         </div>
     );
--- client/src/components/restaurants/Restaurant.js
+++ client/src/components/restaurants/Restaurant.js
@@ -8,39 +8,35 @@ const Restaurant = () => {
     const { restaurants, photoRefs } = restaurantContext;
 
     return (
-        <>
-            <div className='card'>
-                {restaurants.length > 0 && (
-                    <>
-                        <h1 className='text-center'>{restaurants[0].name}</h1>
-                        <img
-                            src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRefs[0]}&key=${process.env.REACT_APP_API_KEY}`}
-                            className='img-fluid'
-                        />
-                        <div className='text-center'>Address</div>
-                        <p className='text-center'>{restaurants[0].vicinity}</p>
-                        <div className='row'>
-                            <div className='col-sm text-center'>Price</div>
-                            <div className='col-sm text-center'>Rating</div>
-                            <div className='col-sm text-center'>
-                                Total Reviews
-                            </div>
+        <div>
+            {restaurants.length > 0 && (
+                <>
+                    <h1 className='text-center'>{restaurants[0].name}</h1>
+                    <img
+                        src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRefs[0]}&key=${process.env.REACT_APP_API_KEY}`}
+                        className='img-responsive'
+                    />
+                    <div className='text-center'>Address</div>
+                    <p className='text-center'>{restaurants[0].vicinity}</p>
+                    <div className='row'>
+                        <div className='col-sm text-center'>Price</div>
+                        <div className='col-sm text-center'>Rating</div>
+                        <div className='col-sm text-center'>Total Reviews</div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-sm text-center'>
+                            {restaurants[0].price_level === 2 && '$$'}
                         </div>
-                        <div className='row'>
-                            <div className='col-sm text-center'>
-                                {restaurants[0].price_level === 2 && '$$'}
-                            </div>
-                            <div className='col-sm text-center'>
-                                {restaurants[0].rating} / 5
-                            </div>
-                            <div className='col-sm text-center'>
-                                {restaurants[0].user_ratings_total}
-                            </div>
+                        <div className='col-sm text-center'>
+                            {restaurants[0].rating} / 5
                         </div>
-                    </>
-                )}
-            </div>
-        </>
+                        <div className='col-sm text-center'>
+                            {restaurants[0].user_ratings_total}
+                        </div>
+                    </div>
+                </>
+            )}
+        </div>
     );
 };
 
