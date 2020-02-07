aca293db6611ccfbcc4f7693561a45b34b074d7d
tonydicker1028 <tonydicker1028@outlook.com>
2020-02-06 18:12:24 -0800

    no restaurant component

--- client/src/components/restaurants/NoRestaurants.js
+++ client/src/components/restaurants/NoRestaurants.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const NoRestaurants = () => {
-    return <div>OUT OF RESTAURANTS</div>;
+    return (
+        <div>
+            <h1 className='text-center text-danger'>OUT OF RESTAURANTS</h1>
+            <div className='text-center'>
+                <iframe
+                    src='https://giphy.com/embed/CDpAmfo9dbOyA'
+                    width='480'
+                    height='360'
+                    frameBorder='0'
+                    class='giphy-embed'
+                    allowFullScreen
+                ></iframe>
+                <p>
+                    <a href='https://giphy.com/gifs/hungry-CDpAmfo9dbOyA'>
+                        via GIPHY
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default NoRestaurants;
